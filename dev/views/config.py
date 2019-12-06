be381287f8953dfc5e5ffecaf284e5c9c3857afd
fKhaleb <kouamfranck96@gmail.com>
2019-12-06 22:16:05 +0100

    Fixing some troubles

--- dev/views/config.py
+++ dev/views/config.py
@@ -1,7 +1,7 @@
 
 
-PYCM_SITE = "https://py-cm.cm"
+PYCM_SITE = "https://py-cm.org"
 
 GITHUB_ACCOUNT = "htpps://github.com/py-cm"
 
-TELEGRAM_GROUP = "https://t.me/pycmuy1"
+TELEGRAM_GROUP = "https://t.me/pythonuy1"
